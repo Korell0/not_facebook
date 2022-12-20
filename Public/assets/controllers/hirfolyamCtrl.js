a562505199790d98316a4a1dd34f0f7260207f49
Logfer <molnar.viktor999@gmail.com>
2022-12-20 13:52:45 +0100

    hirfolyam

--- Public/assets/controllers/hirfolyamCtrl.js
+++ Public/assets/controllers/hirfolyamCtrl.js
@@ -1,3 +1,33 @@
 app.controller('hirfolyamCtrl', function($scope, DB, $rootScope){
+    $scope.hirek = [];
+    $scope.kommentek = [];
 
-})
+    DB.selectAll('posts').then(function(res){
+        $scope.hirek = res.data;
+    })
+
+    DB.selectAll('comments').then(function(res){
+        $scope.kommentek = res.data;
+        console.log($scope.kommentek)
+    })
+    
+
+    $scope.upload = function(){
+        let data = {
+            userID: $rootScope.loggedUser.ID,
+            date: moment(new Date()).format('YYYY-MM-DD'),
+            postmessage: $scope.szoveg
+        }
+        DB.insert('posts', data)
+    }
+
+    $scope.comment = function(index){
+        let data = {
+            userID: $rootScope.loggedUser.ID,
+            postID: index+1,
+            date: moment(new Date()).format('YYYY-MM-DD'),
+            comment: $scope.kommentka
+        }
+        DB.insert('comments', data)
+    }
+});
